26173b449a327e0ffa9dc85d96a722f182d23562
Brandon Gibson <Brandon.gibson.2022@student.lhschools.org>
2020-02-25 14:16:09 -0500

    Added Prompts, initialized variables added equation

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,11 +1,16 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
-
+        drakeEquation();
     }
+
+
+}
 
     public static void drakeEquation(){
 /*Drake equation
@@ -28,11 +33,45 @@ double f_i;
 double f_c;
 int L;
 
-N = rStar * f_p * numberOfExoplanets * f_l * f_i * f_c * L;
+Scanner keyboard;
+keyboard = new Scanner(System.in);
+
+        System.out.println("How many stars do you think form a year?");
+        rStar = keyboard.nextInt();
+        System.out.println("How many Stars do you think will have planets, From .2 to .5?");
+        f_p = keyboard.nextDouble();
+        System.out.println("1 to 5 of those planets will have life on them, pick a number between 1 to 5.");
+        numberOfExoplanets = keyboard.nextInt();
+        System.out.println("What percentage of these will produce life?");
+        f_l = keyboard.nextDouble();
+        System.out.println("What percentage do you think will have intelligent life?");
+        f_i = keyboard.nextDouble();
+        System.out.println("Between .1 and .2 will be able to communicate give me a number .1 or .2");
+        f_c = keyboard.nextDouble();
+        System.out.println("How many years do you think itll take us to find them? between 100 to 100,000,000 years.");
+        L = keyboard.nextInt();
+
+
+
+        //todo initialize the Scanner to take input and commit
+        //ask for rStar number store in the rStar variable and commit
+        //ask for f_p fraction store in the f_p variable and commit
+        //ask for numberOfExoplanets number store in the numberOfplanets variable and commit
+        //ask for f_l fraction store in the f_l variable and commit
+        //ask for f_i fraction store in the f_i variable and commit
+        //ask for f_c fraction store in the f_c variable and commit
+        //ask for L number store in the L variable and commit
+
+
+
+
+N = (int) (rStar * f_p * numberOfExoplanets * f_l * f_i * f_c * L);
 
 
 System.out.println("The number of intelligent Species that are out there \n" +
         "that can communicate with us is: " + N);
+
+
     }
 
 }
